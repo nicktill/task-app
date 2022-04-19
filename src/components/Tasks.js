c7f2e11f687570e52c82a5be90159bd7cee5bb56
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-04-18 22:48:59 -0400

    task app using json server

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,15 +1,13 @@
 import Task from './Task'
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
-    return (
-        <>
-            {tasks.map((task, index) => (
-                // accepting props through props as parameter for function  
-
-                <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
-            ))}
-        </>
-    )
+  return (
+    <>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
+    </>
+  )
 }
 
 export default Tasks
